1f7929ce7b012e66f1474b014f28a5faa26ada57
Rohit Patil <rohitpradeeppatil@gmail.com>
2023-03-27 23:28:35 +0530

    Navbar Added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links } from "./data";
+import "./nav.css";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <h3
+            style={{
+              font: "Montserrat",
+              fontSize: "33px",
+              fontWeight: "700",
+              marginTop: "10px",
+            }}
+          >
+            Medisen
+          </h3>
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+
+              return (
+                <a
+                  to={"#" + url}
+                  key={id}
+                  className={text == "Consult Now" ? "order" : "normal"}
+                >
+                  <li key={id}>{text}</li>
+                </a>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Navbar/data.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/services",
+    text: "Our Services",
+  },
+  {
+    id: 3,
+    url: "/about",
+    text: "about Us",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/order",
+    text: "Consult Now",
+  },
+];
